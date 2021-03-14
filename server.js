cc5eb2707c719f00ea1241e65fdb996b051a6749
mike3983 <mgalla4@gmail.com>
2021-03-14 10:02:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const sport = process.env.SPORT;
+
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log('i love ${sport}');
     await sleep(5000);
   }
 }
